a9132cb39d8dd3cb4dabd02a5cd5a23332865990
siorai <siorai@gmail.com>
2018-02-03 11:28:40 -0800

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <v-app dark>
+      <v-navigation-drawer
+        clipped
+        fixed
+        v-model="drawer"
+        app
+      >
+        <v-list dense>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>DancesafeResults</v-toolbar-title>
+      </v-toolbar>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
